db7c43079babf2c7d3d3314f2858d099ed9659df
Noah Jahn <najahn19@gmail.com>
2024-07-26 00:20:37 -0400

    Add error handling response to the endpoint

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,28 @@ import type { GenerateTypesOptions } from './services/TypesService';
 
 export default defineEndpoint((router, { getSchema, logger }) => {
     router.get('/', async (req, res) => {
-        res.type('text/plain');
-        res.status(200);
+        try {
+            res.type('text/plain');
+            res.status(200);
 
-        const options: GenerateTypesOptions = {
-            spaces: req.query.spaces
-                ? Math.floor(parseInt(req.query.spaces?.toString()))
-                : 2,
-            useTabs: req.query.useTabs === 'true',
-            trailingSemicolons: req.query.trailingSemicolons === 'true',
-        };
-        await generateTypes(getSchema, logger, options).then((text) =>
-            res.send(text),
-        );
+            const options: GenerateTypesOptions = {
+                spaces: req.query.spaces
+                    ? Math.floor(parseInt(req.query.spaces?.toString()))
+                    : 2,
+                useTabs: req.query.useTabs === 'true',
+                trailingSemicolons: req.query.trailingSemicolons === 'true',
+            };
+            await generateTypes(getSchema, logger, options).then((text) =>
+                res.send(text),
+            );
+        } catch (error) {
+            logger.error(error);
+            res.status(500);
+            if (error instanceof Error) {
+                res.send(error.message);
+                return;
+            }
+            res.send(JSON.stringify(error));
+        }
     });
 });
